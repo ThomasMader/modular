4e9650be5876a48ca99089e05353af8a1f59e8f7
Thomas Mader <dev@unplex.eu>
2025-10-25 11:20:55 +0200

    Cleanup and improve flake/shell

--- flake.nix
+++ flake.nix
@@ -3,26 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
 
-  # flake-compat is needed for shell.nix
-  outputs = { self, nixpkgs, flake-utils, flake-compat }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-      ca-bundle-path = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
-      fhs = pkgs.buildFHSEnv {
-        name = "fhs-shell";
-        targetPkgs = pkgs: with pkgs; [ bash perl curl cacert python3 pixi zlib libxml2_13 ];
-        profile = ''
-          export SSL_CERT_FILE=${ca-bundle-path}
-          export NIX_SSL_CERT_FILE=SSL_CERT_FILE;
-        '';
-      };
+  outputs = { self, nixpkgs }:
+    let
+      systems = nixpkgs.lib.systems.flakeExposed;
     in {
-      devShells.default = fhs.env;
-    });
+      devShells = nixpkgs.lib.genAttrs systems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          fhs = pkgs.buildFHSEnv {
+            name = "fhs-shell";
+            targetPkgs = pkgs: with pkgs; [
+              bash perl curl python3 pixi zlib libxml2
+              #libxml2_13 cacert
+            ];
+            runScript = "bash";
+            profile = "";
+          };
+        in {
+          default = fhs.env;
+        }
+      );
+    };
 }
+
